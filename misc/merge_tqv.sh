15c4c1683b83ed5ffa4eeac87aea5924e5fddb3c
Hyunju Jung <hyunju.jung@kit.edu>
2023-05-31 12:52:24 +0200

    fixed grid des

--- misc/merge_tqv.sh
+++ misc/merge_tqv.sh
@@ -5,36 +5,31 @@ module load hdf5-blosc
 
 opath="/archive/meteo/w2w-p2/B6/high_resolution/"
 
-#spinup files
-#for FILENAME in $(ls -lX ${opath}output_spinnup/nature_run_DOM01_ML_00??.nc | head -72)
-#do
-#    echo ${FILENAME}
-#    num=${FILENAME:(-27)}
-#    cdo selvar,tqv ${FILENAME} data/${num}    
-#done
-
 #some have different remapped grid in the spinup files
-#for FILENAME in $(ls -lX data/nature_run_DOM01_ML_00??.nc | head -49)
-#do
-#    echo ${FILENAME}
-#    num=${FILENAME:(-27)}
-#    cdo -P 8 remapcon,grid_des.txt ${FILENAME} data/remap_${num}    
-#done
-
-#change remap filename
-for FILENAME in $(ls -lX data/remap_nature_run_DOM01_ML_00??.nc)
+for FILENAME in $(ls -lX ${opath}output_spinnup/nature_run_DOM01_ML_00??.nc | head -49)
 do
+    echo ${FILENAME}
     num=${FILENAME:(-27)}
-    mv ${FILENAME} data/${num}
-    echo "data/"${num}
-    
+    cdo selvar,tqv ${FILENAME} data/selvar_${num}
+    cdo -P 8 remapcon,grid_des.txt data/selvar_${num} data/${num}    
+done
+
+rm data/selvar_*.nc
+
+#spinup files
+for FILENAME in $(ls -lX ${opath}output_spinnup/nature_run_DOM01_ML_{50..72}.nc)
+do
+    echo ${FILENAME}
+    num=${FILENAME:(-27)}
+    cdo selvar,tqv ${FILENAME} data/${num}    
 done
 
 #analysis period
 #for FILENAME in $(ls -lX ${opath}param/nature_run_DOM01_ML_????.nc | tail -962)
-#do
-#    num=${FILENAME:(-27)}
-#    cdo selvar,tqv ${FILENAME} data/${num}    
-#done
+for FILENAME in $(ls -lX ${opath}param/nature_run_DOM01_ML_00{73..99}.nc)
+do
+    num=${FILENAME:(-27)}
+    cdo selvar,tqv ${FILENAME} data/${num}    
+done
 
-#cdo --no_history mergetime data/nature_run_DOM01_ML_????.nc data/tqv-5km-param.nc
+cdo --no_history mergetime data/nature_run_DOM01_ML_????.nc data/tqv-5km-param.nc
